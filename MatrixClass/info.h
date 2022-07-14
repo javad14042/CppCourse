7377f2de29ee9565f47e379501cf5ca5a7bc26c5
amirreza ebadifar <amirrezaebr@gmail.com>
2022-07-14 16:07:36 +0430

    Adding move constructor and copy constructor

--- MatrixClass/info.h
+++ MatrixClass/info.h
@@ -1,20 +1,24 @@
 #ifndef INFO_H_INCLUDED
 #define INFO_H_INCLUDED
 
-class matrix
-{
+class matrix {
 public:
 
     const int rows;
     const int columns;
     int **Dimensions;
 
-    explicit matrix(const int rows=2,const int columns=2);
+    explicit matrix(const int rows = 2, const int columns = 2);
+    matrix(const matrix &source);
+    matrix(matrix &&source);
+    ~matrix();
+
     void GetElements();
     void PrintfMatrix();
     void multiplication_by_a_number(int Input);
-    void multiplication_of_two_matrices(matrix a,matrix b);
-   ~matrix();
+    void multiplication_of_two_matrices(matrix a, matrix b);
+    int determinantOfMatrix();
+
 
 };
 
--- MatrixClass/main.cpp
+++ MatrixClass/main.cpp
@@ -6,13 +6,9 @@ using namespace std;
 int main()
 {
 
-    matrix sample1(3,3);
-    sample1.GetElements();
-    sample1.PrintfMatrix();
-    sample1.multiplication_by_a_number(2);
-    matrix sample2(3,3);
-    sample2.multiplication_of_two_matrices(sample1,sample2);
-
+matrix sample1(3,3);
+sample1.GetElements();
+cout<<sample1.determinantOfMatrix();
 
     return 0;
 }
