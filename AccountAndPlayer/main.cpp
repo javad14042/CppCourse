dff31ea2272c0db9acb9295c11fbed611b5056a7
Javad Agheleh <32520902+javad14042@users.noreply.github.com>
2022-07-20 20:49:14 +0430

    Adding login and signup
    Co-authored-by: amirreza ebadifar <amirrezaebr@gmail.com>

--- AccountAndPlayer/main.cpp
+++ AccountAndPlayer/main.cpp
@@ -1,47 +1,101 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include "account.h"
 #include "management.h"
 
 using namespace std;
 
-int main()
-{
-    management series1;
-    ifstream myfile("filename.txt");
-    string tmp1;
-    long int tmp2;
-    long int tmp3;
-    long int tmp4;
+int main() {
 
-    if(!myfile.is_open()){
-        cout << "file not found" << endl;
-        return 1;
+
+    management branch1;
+/*branch1.readFile();
+branch1.ListOfAccounts();*/
+
+    account amir;
+    amir.setUsername("amir");
+    amir.setPass("123");
+    amir.setName("amir");
+    branch1.addToVector(amir);
+
+    account reza;
+    reza.setUsername("reza");
+    reza.setPass("321");
+    reza.setName("reza");
+    branch1.addToVector(reza);
+    string sop;
+    char op;
+    string InUsername;
+    string InPass;
+
+    while (1) {
+        cout << "enter l for login or s for sign up e for exit\n";
+        cin >> sop;
+        op = sop[0];
+        if (op == 'l' || op == 'L') {
+            cout << "enter your username :\n";
+            cin >> InUsername;
+            cout << "enter your password :\n";
+            cin >> InPass;
+            if (branch1.loginCheck(InUsername, InPass)) {
+                int index = branch1.returnIndex(InUsername);
+                while (1) {
+                    cout << "Enter d for deposit w for withdraw b for displaying balance e for exit\n";
+                    cin >> op;
+                    if (op == 'd' || op == 'D') {
+                        cout << "How much do you want to deposit?\n";
+                        int input;
+                        cin >> input;
+                        branch1.deposit(input, index);
+                    } else if (op == 'w' || op == 'W') {
+                        cout << "How much do you want to withdraw?\n";
+                        int input;
+                        cin >> input;
+                        branch1.withdraw(input, index);
+                    } else if (op == 'b' || op == 'B')
+                        branch1.DisplayBalance(index);
+                    else if (op == 'e' || op == 'E')
+                        break;
+                    else
+                        cout << "Wrong Input\n";
+                }
+            }
+
+        } else if (op == 's' || op == 'S') {
+            string InName;
+            long int PhoneNumber;
+            cout << "Please enter your name\n";
+            cin >> InName;
+            cout << "Please enter your phone number\n";
+            cin >> PhoneNumber;
+            cout << "enter your username :\n";
+            cin >> InUsername;
+            while (!branch1.SignUpCheck(InUsername)) {
+                cout << "Username already taken\n";
+                cout << "Enter another username\n";
+                cin >> InUsername;
+            }
+            cout << "enter your password :\n";
+            cin >> InPass;
+            while (!management::PassCheck(InPass)) {
+                cout << "Your password is too short\n";
+                cout << "Enter another password\n";
+                cin >> InPass;
+            }
+            account tmp(InName, PhoneNumber);
+            tmp.setUsername(InUsername);
+            tmp.setPass(InPass);
+            branch1.addToVector(tmp);
+            cout << "Your account is registered successfully" << endl;
+            cout << "You can login to your account" << endl;
+        } else if (op == 'e' || op == 'E')
+            break;
+        else
+            cout << "Wrong Input\n";
     }
-
-    while(myfile >> tmp1 >> tmp2 >> tmp3 >> tmp4)
-    {
-//        account tmp;
-        series1.customers.emplace_back(tmp1,tmp2,tmp3,tmp4);
-    }
-    myfile.close();
-    int id  = 124;
-    series1.search_id(id);
-    series1.deposit(id,600);
-    series1.search_id(id);
-
-    account tmp;
-    string name = "tmpName";
-//    account tmp2(name);
-//    account tmp3(name,30);
-    series1.addToVector(account(name)); //explicit
-//    series1.addToVector(name); //implicit
-
-
-
 
     return 0;
 }
+
 
 
